296a2c7a04e61fc0abbf0022974912953e1aa5af
Khummeung Wattanasaroj <62160244@go.buu.ac.th>
2022-10-23 14:20:55 +0700

    Update MarketAdapter

--- app/src/main/java/com/defalt/cryptocoinex/adapter/MarketAdapter.kt
+++ app/src/main/java/com/defalt/cryptocoinex/adapter/MarketAdapter.kt
@@ -1,5 +1,54 @@
 package com.defalt.cryptocoinex.adapter
 
-class MarketAdapter {
-    
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.defalt.cryptocoinex.R
+import com.defalt.cryptocoinex.databinding.CurrencyItemLayoutBinding
+import com.defalt.cryptocoinex.model.CryptoCurrency
+
+class MarketAdapter(var context : Context, var list : List<CryptoCurrency>) : RecyclerView.Adapter<MarketAdapter.MarketViewHolder>(){
+
+    inner class MarketViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        var binding = CurrencyItemLayoutBinding.bind(view)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarketViewHolder {
+        return MarketViewHolder(LayoutInflater.from(context).inflate(R.layout.currency_item_layout, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: MarketViewHolder, position: Int) {
+        val item = list[position]
+        holder.binding.currencyNameTextView.text = item.name
+        holder.binding.currencySymbolTextView.text = item.symbol
+
+        Glide.with(context).load(
+            "https://s2.coinmarketcap.com/static/img/coins/64x64/" + item.id + ".png"
+        ).thumbnail(Glide.with(context).load(R.drawable.spinner))
+            .into(holder.binding.currencyImageView)
+
+        Glide.with(context).load(
+            "https://s3.coinmarketcap.com/generated/sparklines/web/7d/usd/" + item.id + ".png"
+        ).thumbnail(Glide.with(context).load(R.drawable.spinner))
+            .into(holder.binding.currencyImageView)
+
+        holder.binding.currencyPriceTextView.text = item.quotes[0].price.toString()
+
+        if (item.quotes!![0].percentChange24h > 0){
+            holder.binding.currencyChangeTextView.setTextColor(ContextCompat.getColor(context,R.color.green))
+            holder.binding.currencyChangeTextView.text = "+ ${String.format("%.2f",item.quotes[0].percentChange24h)} %"
+        }else{
+            holder.binding.currencyChangeTextView.setTextColor(ContextCompat.getColor(context,R.color.red))
+            holder.binding.currencyChangeTextView.text = "${String.format("%.2f",item.quotes[0].percentChange24h)} %"
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
 }
